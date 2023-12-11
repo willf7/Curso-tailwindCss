d22050b83aac1f09a1d449f53129c3003fe824c8
William Felipe <williamfelipe081@gmail.com>
2023-12-11 16:58:04 -0300

    Correcao da aula-03 que estava bugada

---
+++ tailwind-aula-03/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+<<<<<<< HEAD
+  content: [],
+=======
+  content: ["src/*.{html, js}"],
+>>>>>>> 47d8441e2c20a63033b357814e4d4620c3090cf1
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+<<<<<<< HEAD
+}
+
+=======
+};
+>>>>>>> 47d8441e2c20a63033b357814e4d4620c3090cf1
